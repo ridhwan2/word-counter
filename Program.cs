41088b390fb17562b85beb5eae6d50d1ab842212
ridhwan2 <110493782+ridhwan2@users.noreply.github.com>
2022-08-04 20:46:47 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Title = "waiting";
+Console.ForegroundColor = ConsoleColor.Red;
+
+string word = Console.ReadLine();
+int length = word.Split(' ').Length;
+
+Console.Title = "finished!";
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.WriteLine();
+Console.WriteLine(length + " words");
+
+Thread.Sleep(5000);
+Environment.Exit(1);
